9f909ad69f843b648a3692ed23e3508c4ac20977
JaeHyeon_Lee <project0858@gmail.com>
2024-11-27 21:24:42 +0900

    [add][BOJ]누적합: 그림을 그려 어느 인덱스에 -1을 해야하는지 파악

---
+++ JaeHyeonLee/BOJ/boj11659_S3_구간합구하기4.py
@@ -0,0 +1,46 @@
+# region 문제 정리
+"""
+입력
+N : 주어지는 수의 개수
+M : 목표 값의 횟수(반복의 횟수)
+i, j : i 부터 j 까지의 합(인덱스 아님)
+
+출력
+i 부터 j 까지의 합
+
+
+누적합
+
+누적합 배열
+반복문을 돌며
+  누적합을 계산
+  값 출력
+"""
+
+
+# endregion
+# region 풀이
+
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+acc_lst = [0]
+for n in nums:
+  acc_lst.append(acc_lst[-1] + n)
+  
+for i in range(M):
+  i, j = map(int, input().split())
+  print(acc_lst[j] - acc_lst[i - 1])
+
+# endregion
+
+# region 깨달은 점
+'''
+그림을 그리면 어느 인덱스에 -1을 해야하는지 빠르게 파악가능
+'''
+
+
+# endregion
