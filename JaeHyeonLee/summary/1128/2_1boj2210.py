8e9ac2d906a2025964b8b8de8f39e35472e9ed6c
JaeHyeon_Lee <project0858@gmail.com>
2024-12-02 09:08:43 +0900

    [add][CIC] 외판원2 \ for문을 통해 방문하는 위치는 해당 도시까지 가는데 드는 비용임\ 그 비용이 들게 한 도시로 인덱스를 옮겨 \ 다시 순회하며 이동가능한 도시를 탐색하는 것

--- JaeHyeonLee/summary/1128/2_1boj2210.py
+++ JaeHyeonLee/summary/1128/2_1boj2210.py
@@ -38,3 +38,11 @@ for i in range(5):
 print(len(ans))
 
 # endregion
+
+
+
+
+
+
+
+
--- JaeHyeonLee/summary/1128/3_1boj10971.py
+++ JaeHyeonLee/summary/1128/3_1boj10971.py
@@ -1,36 +1,95 @@
-# region 
-'''
-가중치가 들어간 인접행렬
-문제에 주어진 입력값이 구조화 되어 있으므로
-문제만 풀면됨
+# # region
+# """
+# 가중치가 들어간 인접행렬
+# 문제에 주어진 입력값이 구조화 되어 있으므로
+# 문제만 풀면됨
 
-마지막에서 출발지로 가는 경로 + 1
+# 마지막에서 출발지로 가는 경로 + 1
 
-0에서 출발하는 순환 경로 모두 탐색
-순환이기 때문에 중복됨
-'''
+# 0에서 출발하는 순환 경로 모두 탐색
+# 순환이기 때문에 중복됨
+# """
 
-# endregion
+# # endregion
 
-# region 풀이
+# # region 풀이
+
+# N = int(input())
+# adj_matrix = [list(map(int, input().split())) for _ in range(N)]
+
+
+# def perm(now, depth, cost):
+#     # 마지막 도시에 도착하면(depth == N)
+#     if depth == N:
+#         # 마지막 - 출발 가는 비용을 더해주고
+#         # cost += adj_matrix[now][0]
+#         # 정답 갱신 하고 리턴
+#         ans = min(ans, cost)
+#         return
+#     if cost >= ans:
+#         return
+
+#     # 갈 수 있는 곳을 탐색하여
+#     for after in range(N):
+#         # 방문한적 없고, 0이 아니라면
+#         if not visited[after] and adj_matrix[now][after]:
+#             print(f'{now} : {after} // {adj_matrix[now][after]}')
+#             # 방문 체크하고
+#             visited[after] = 1
+#             # 방문(재귀)
+#             perm(after, depth + 1, cost + adj_matrix[now][after])
+#             # 방문 체크 해제
+#             visited[after] = 0
+
+
+# # 세팅(출발지, 예정지, 도착지)
+# # dfs 예정지 없어도 됨
+# visited = [0] * N
+# ans = float("INF")
+
+# visited[0] = 1
+# perm(0, 1, 0)
+
+
+# # endregion
+
+
+import sys
+
+input = sys.stdin.readline
+
+
+def dfs(now, cost, depth):
+    global ans
+
+    if depth == N:
+        if cost_matrix[now][0]:
+            cost += cost_matrix[now][0]
+            ans = min(ans, cost)
+            return
+        else:
+            return
+
+    if cost > ans:
+        return
+
+    for after in range(N):
+        print(f"before // {now} : {after} , depth : {depth}")
+        if not check[after] and cost_matrix[now][after]:
+            print(f"{now} : {after} // {cost_matrix[now][after]}")
+            check[after] = 1
+            dfs(after, cost + cost_matrix[now][after], depth + 1)
+            check[after] = 0
+            print()
+
 
 N = int(input())
-adj_matrix = [list(map(int, input().split())) for _ in range(N)]
+cost_matrix = [list(map(int, input().split())) for _ in range(N)]
 
-def perm(depth):
-  # 마지막 도시에 도착하면(depth == N)
-    # 마지막 - 출발 가는 비용을 더해주고
-    # 정답 갱신 하고 리턴
-  
-  # 갈 수 있는 곳을 탐색하여
-    # 방문한적 없고, 0이 아니라면
-      # 방문 체크하고
-      # 방문(재귀)
-      
-      # 방문 체크 해제
-      pass
-visited = [0] * N
+check = [0] * N
+ans = 987654321
 
-perm(0)
+check[0] = 1
+dfs(0, 0, 1)
 
-# endregion
+print(ans)
