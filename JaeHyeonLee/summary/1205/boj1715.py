5bb5c2717e28b9d4999e98a3ff755e1791008f56
JaeHyeon_Lee <project0858@gmail.com>
2024-12-06 13:06:28 +0900

    [add][BOJ] 최소힙 항상 최소값을 꺼냄

--- JaeHyeonLee/summary/1205/boj1715.py
+++ JaeHyeonLee/summary/1205/boj1715.py
@@ -6,14 +6,13 @@ heap = []
 for _ in range(N):
     card = int(input())
     heappush(heap, card)
-    # 카드뭉치가 1개 남을 때가지 반복
+    # 카드뭉치가 1개 남을 때까지 반복
 
 ans = 0
 
 while len(heap) > 1:
-    # 가장 작은 두 값을 합치고
     c1, c2 = heappop(heap), heappop(heap)
-    # 힙에 넣는다
+    # 가장 작은 두 값을 합치고
     c = c1 + c2
     # 더한 값(비교횟수)를 누적
     ans += c
