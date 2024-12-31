37e23c39bd0c6eb8039a0af4f42ce7c2024146b2
JaeHyeon_Lee <project0858@gmail.com>
2024-12-31 09:26:50 +0900

    [modify][SWAE] 주석 추가

--- JaeHyeonLee/SWEA/1954.D2_달팽이숫자.py
+++ JaeHyeonLee/SWEA/1954.D2_달팽이숫자.py
@@ -11,21 +11,28 @@ N * N
 
 
 def snail(N):
+    # 초기화화
     lst = [([0] * N) for _ in range(N)]
     # 우 하 좌 상
     dr = [0, 1, 0, -1]
     dc = [1, 0, -1, 0]
+    # direction index
     d = 0
+    # 시작 지점
     r, c = 0, 0
     cnt = 1
     while cnt <= N * N:
+        # 범위 안에 있고 0이면
         if 0 <= r < N and 0 <= c < N and lst[r][c] == 0:
             lst[r][c] = cnt
             cnt += 1
         else:
+            # 이전에 더해진 값 빼고
             r -= dr[d]
             c -= dc[d]
+            # 방향 전환
             d = (d + 1) % 4
+        # 이동
         r += dr[d]
         c += dc[d]
 
