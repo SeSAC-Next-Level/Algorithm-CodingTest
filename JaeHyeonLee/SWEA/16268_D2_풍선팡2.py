3a986ca22b71a9df3670ed6487cf0983e6cf256b
JaeHyeon_Lee <project0858@gmail.com>
2025-01-07 13:07:08 +0900

    [add][NL] 풍선팡2 델타탐색 주어진 공간을 벗어나지 않도록 조건문이 추가되어야 함 소요시간 : 20분

--- JaeHyeonLee/SWEA/16268_D2_풍선팡2.py
+++ JaeHyeonLee/SWEA/16268_D2_풍선팡2.py
@@ -1,13 +1,23 @@
 T = int(input())
 
-for tc in range(1, T+1):
-  N, M = map(int, input().split())
-  matrix = [list(map(int, input().split())) for _ in range(N)]
-  # 정답 초기화
-  
-  
-  # 순회하면서
-    # tmp = 0
-    # r, c에서 델타탐색(4방향)
-    
+
+dr = [0, 0, 1, -1]
+dc = [1, -1, 0, 0]
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(N)]
+
+    # 정답 초기화
+    ans = 0
+    # 순회하면서
+    for r in range(N):
+        for c in range(M):
+            tmp = matrix[r][c]
+            # r, c에서 델타탐색(4방향)
+            for d in range(4):
+                nr, nc = r + dr[d], c + dc[d]
+                if 0 <= nr < N and 0 <= nc < M:
+                    tmp += matrix[nr][nc]
     # ans
+            ans = max(ans, tmp)
+    print(f'#{tc} {ans}')
