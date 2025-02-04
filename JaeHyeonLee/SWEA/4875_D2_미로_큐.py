27296c8577c504cdbb39e626e4db9e302c625e40
JaeHyeon_Lee <project0858@gmail.com>
2025-02-04 12:46:15 +0900

    [modify]변수명 수정

--- JaeHyeonLee/SWEA/4875_D2_미로_큐.py
+++ JaeHyeonLee/SWEA/4875_D2_미로_큐.py
@@ -18,7 +18,7 @@ dc = [1, -1, 0, 0]
 
 for ts in range(1, T + 1):
     정점 = int(input())
-    인접리스트 = [list(map(int, input().rstrip())) for _ in range(정점)]
+    인접행렬 = [list(map(int, input().rstrip())) for _ in range(정점)]
 
 
     ans = 0
@@ -27,7 +27,7 @@ for ts in range(1, T + 1):
     # 방문지 = set()
     for r in range(정점):
         for c in range(정점):
-            if 인접리스트[r][c] == 2:
+            if 인접행렬[r][c] == 2:
                 # 방문지.add((r, c))
                 예정지.append((r, c))
                 break
@@ -36,14 +36,14 @@ for ts in range(1, T + 1):
     
     while 예정지:
         r, c = 예정지.popleft()
-        if 인접리스트[r][c] == 3:
+        if 인접행렬[r][c] == 3:
             ans = 1
             break
-        인접리스트[r][c] = 1 # 방문지를 대신할 수 있음
+        인접행렬[r][c] = 1 # 방문지를 대신할 수 있음
         for n in range(4):
             nr, nc =  r + dr[n], c + dc[n]
-            # if 0 <= nr < 정점 and 0 <= nc < 정점 and 인접리스트[nr][nc] != 1 and (nr, nc) not in 방문지 :
-            if 0 <= nr < 정점 and 0 <= nc < 정점 and 인접리스트[nr][nc] != 1 :
+            # if 0 <= nr < 정점 and 0 <= nc < 정점 and 인접행렬[nr][nc] != 1 and (nr, nc) not in 방문지 :
+            if 0 <= nr < 정점 and 0 <= nc < 정점 and 인접행렬[nr][nc] != 1 :
                 예정지.append((nr, nc))
                 # 방문지.add((nr, nc))
                 
